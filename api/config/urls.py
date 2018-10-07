afef73553f3eaf24bed59b196881ccdb103b1b98
tanvir hasan <tanvir002700@gmail.com>
2018-10-07 20:42:27 +0600

    add api auth url

--- api/config/urls.py
+++ api/config/urls.py
@@ -1,7 +1,7 @@
-"""api URL Configuration
+"""config URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -20,24 +20,31 @@ from django.conf.urls import include
 from rest_framework.schemas import get_schema_view
 from rest_framework.documentation import include_docs_urls
 from django.conf.urls.static import static
+
+from auth.views import AuthApiListView
 from utils.router import DefaultRouterWithAPIViews
 
 admin.site.site_header = settings.ADMIN_SITE_HEADER
 
 router = DefaultRouterWithAPIViews()
-
+router.register('api/auth', AuthApiListView)
 
 schema_view = get_schema_view(title=settings.API_BROWSER_HEADER, public=True)
 doc_urls = include_docs_urls(title=settings.API_BROWSER_HEADER)
 api_browser_urls = include('rest_framework.urls')
-#auth_urls = include('auth.urls')
+auth_urls = include('auth.urls')
 
 urlpatterns = [
     path('api/', doc_urls),
-    #path('api/auth/', auth_urls),
+    path('api/auth/', auth_urls),
     path('api/schema/', schema_view),
     path('api/browser/', api_browser_urls),
     path('api/admin/', admin.site.urls),
 ]
 
 urlpatterns += router.urls
+
+# Serve media assets for development, only works while DEBUG=True
+# https://docs.djangoproject.com/en/2.0/howto/static-files/#serving-files-uploaded-by-a-user-during-development
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
